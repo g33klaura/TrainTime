1142cfedd6a1acb46f2ca0dd6153b5d8fa48e5e6
Laura Boetcher <g33klaura@gmail.com>
2017-09-01 18:19:09 -0400

    Initial Firebase object set and testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,4 @@
-// Testing moment.js
-var a = moment().format('LLLL');
-	console.log(a);
-
-// 'LLLL' = Friday, September 1, 
-// Other formatting on moment.js guide
-
+// JavaScript for 'TrainTime' Assignment
 // Steps to complete:
 // [x] Setup Firebase app to store data
 // [] When adding trains, admin users can submit:
@@ -16,22 +10,48 @@ var a = moment().format('LLLL');
 // [] Calculates when next train will arrive (relative to the current time)
 // [] Users from multiple machines can view same train times
 
+// Initialize Firebase
+var config = {
+	apiKey: 'AIzaSyBFCC6me05S9bj-uKClv29LHwBZV0jDnHc',
+    authDomain: 'train-time-6f8ee.firebaseapp.com',
+    databaseURL: 'https://train-time-6f8ee.firebaseio.com',
+    projectId: 'train-time-6f8ee',
+    storageBucket: '',
+    messagingSenderId: '787951890946'
+};
+firebase.initializeApp(config);
+
 
 
 
 // VARIABLES ====================
 // 
 
-// Initialize Firebase
-var config = {
-	apiKey: "AIzaSyBFCC6me05S9bj-uKClv29LHwBZV0jDnHc",
-    authDomain: "train-time-6f8ee.firebaseapp.com",
-    databaseURL: "https://train-time-6f8ee.firebaseio.com",
-    projectId: "train-time-6f8ee",
-    storageBucket: "",
-    messagingSenderId: "787951890946"
-};
-firebase.initializeApp(config);
+var database = firebase.database();
+
+var trainName = '';
+var destination = '';
+var freq = '';
+var next = '';
+var minAway = '';
+
+// Testing moment.js
+var a = moment().format('LLLL');
+	console.log(a);
+
+
+// Following Firebase tutorial
+// Get elements
+var preObject = document.getElementById('object');
+// Create references
+// Referencing Firebase OBJECT**
+// var dbRefObject = firebase.database().ref().child('object');
+var dbRefObject = firebase.database().ref()
+// Synce object changes
+dbRefObject.on('value', snap => console.log(snap.val()));
+
+
+
 
 
 
