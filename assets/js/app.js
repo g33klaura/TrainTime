b2865b7095d0c0f8ac0de62a03677f0ff7ca5ac2
Laura Boetcher <g33klaura@gmail.com>
2017-09-01 16:33:15 -0400

    Pseudo-code steps to complete

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+// Testing moment.js
+var a = moment().format('LLLL');
+	console.log(a);
+
+// 'LLLL' = Friday, September 1, 
+// Other formatting on moment.js guide
+
+// Steps to complete:
+// [] Setup Firebase app* to store data
+// [] When adding trains, admin users can submit:
+// [] Train Name
+// [] Destination
+// [] First Train Time
+// 	[] (in military time)
+// [] Frequency of scheduled train (in minutes)
+// [] Calculates when next train will arrive (relative to the current time)
+// [] Users from multiple machines can view same train times
+
